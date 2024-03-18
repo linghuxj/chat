5bc9e7c366e96a68c20dfef5464fda7848c4143d
linghuxj <linghuxj@gmail.com>
2024-03-18 19:51:56 +0800

    修复bug

--- src/const/market.ts
+++ src/const/market.ts
@@ -7,6 +7,7 @@ export const DEFAULT_AGENTS_MARKET_ITEM: AgentsMarketItem = {
   author: '',
   config: DEFAULT_AGENT_CONFIG,
   content: '',
+  comments: [],
   createAt: Date.now().toString(),
   homepage: '',
   identifier: '',
