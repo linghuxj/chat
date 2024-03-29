a8c07ea5255dfb7d4dbca23af10e95da748a9785
linghuxj <linghuxj@gmail.com>
2024-03-29 08:42:53 +0800

    修改首页内容

--- src/app/welcome/features/Banner/Hero.tsx
+++ src/app/welcome/features/Banner/Hero.tsx
@@ -6,6 +6,8 @@ import {useTranslation} from 'react-i18next';
 
 import {genSize, useStyles} from './style';
 import {Typography} from "antd";
+import {Flexbox} from "react-layout-kit";
+import {useRouter} from "next/navigation";
 
 const {Link} = Typography;
 
@@ -15,8 +17,8 @@ const {Link} = Typography;
 const Hero = memo<{ mobile?: boolean; width: number }>(({width, mobile}) => {
   const size: any = {
     base: genSize(width / 3.5, 240),
-    desc: genSize(width / 50, 14),
-    desc2: genSize(width / 65, 12),
+    desc: genSize(width / 50, 18),
+    desc2: genSize(width / 65, 13),
     logo: genSize(width / 2.5, 180),
     title: genSize(width / 20, 32),
   };
@@ -27,6 +29,7 @@ const Hero = memo<{ mobile?: boolean; width: number }>(({width, mobile}) => {
   const {styles} = useStyles(size.base);
 
   const {t} = useTranslation('welcome');
+  const router = useRouter();
 
   return (
     <>
@@ -41,19 +44,24 @@ const Hero = memo<{ mobile?: boolean; width: number }>(({width, mobile}) => {
       {/*  {mobile ? <LogoThree size={size.logo} /> : <LogoSpline height={'100%'} width={'100%'} />}*/}
       {/*</Flexbox>*/}
       <div className={styles.desc} style={{fontSize: size.desc}}>
-        {t('slogan.desc')}
+        XY银行<Link className={styles.desc2} style={{fontSize: size.desc}} onClick={() => router.push('/settings/intro')}
+                    target={'_blank'}>智能运营官</Link>
       </div>
-      <div className={styles.desc} style={{fontSize: size.desc}}>
-        {t('slogan.desc1')}
-      </div>
+      {/*<div className={styles.desc} style={{fontSize: size.desc}}>*/}
+      {/*  {t('slogan.desc1')}*/}
+      {/*</div>*/}
       <div className={styles.title} style={{fontSize: size.title}}>
         <strong style={mobile ? {fontSize: '1.4em'} : {}}>ChatBank</strong>
         {/*{mobile ? <br /> : ' '}*/}
         {/*{t('slogan.title')}*/}
       </div>
-      <Link className={styles.desc2} style={{fontSize: size.desc2}} target={'_blank'}>
-        @{t('slogan.desc2')}
-      </Link>
+      <Flexbox className={styles.desc} style={{fontSize: size.desc2, padding: '16px'}} align={'start'}>
+        <div>最新汇报:</div>
+        <div>入职手机银行App到岗第三天开始，每天因我打开XY银行App的客户数30+人，其中15人为XY银行新客户；</div>
+        <div>10天后为为参与ZH项目，存入保证金，增加存款；</div>
+        <div>20天后，为开展HZ项目，多人贷款获得批准。</div>
+        <Link className={styles.desc2} target={'_blank'} onClick={() => router.push('/market?agent=ID-0002')}>查看详情</Link>
+      </Flexbox>
     </>
   );
 });
